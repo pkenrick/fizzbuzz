d43ac676ae703151b5ddaad01f3226b7b4ef8f3d
paulkenrick <pkenrick@gmail.com>
2016-06-21 13:07:55 +0100

    Completed FizzBuzz

--- jasmine-standalone-2.4.1/spec/fizzBuzzSpec.js
+++ jasmine-standalone-2.4.1/spec/fizzBuzzSpec.js
@@ -7,16 +7,62 @@ describe('Javabuzz',function(){
 	describe('knows when the number is',function(){
 		it('divisible by 3',function(){
 			javabuzz = new Javabuzz();
-			expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+			expect(javabuzz.isDivisibleBy(3, 3)).toBe(true);
+		});
+
+		it('divisible by 5',function(){
+			javabuzz = new Javabuzz();
+			expect(javabuzz.isDivisibleBy(5, 5)).toBe(true);
+		});
+
+		it('divisible by 15',function(){
+			javabuzz = new Javabuzz();
+			expect(javabuzz.isDivisibleBy(15, 15)).toBe(true);
 		});
 	});
 
 	describe('knows when the number is NOT',function(){
 		it('divisible by 3',function(){
 			javabuzz = new Javabuzz();
-			expect(javabuzz.isDivisibleByThree(4)).toBe(false);
+			expect(javabuzz.isDivisibleBy(3, 4)).toBe(false);
+		});
+
+		it('divisible by 5',function(){
+			javabuzz = new Javabuzz();
+			expect(javabuzz.isDivisibleBy(5, 4)).toBe(false);
+		});
+
+		it('divisible by 15',function(){
+			javabuzz = new Javabuzz();
+			expect(javabuzz.isDivisibleBy(15, 4)).toBe(false);
 		});
 	});
+
+	describe('prints', function() {
+		it('"Fizz" when divisible by 3', function() {
+			expect(javabuzz.checkNumber(3)).toEqual("Fizz")
+		});
+
+		it('"Buzz" when divisible by 5', function() {
+			expect(javabuzz.checkNumber(5)).toEqual("Buzz")
+		});
+
+		it('"FizzBuzz" when divisible by 15', function() {
+			expect(javabuzz.checkNumber(15)).toEqual("FizzBuzz")
+		});
+
+		it('the number when not divisible by any', function() {
+			expect(javabuzz.checkNumber(4)).toEqual(4)
+		});
+
+		it('"0" when number is 0', function() {
+			expect(javabuzz.checkNumber(0)).toEqual(0)
+		});
+
+	});
+
+		
+	
 
 
 
--- jasmine-standalone-2.4.1/src/fizzBuzz.js
+++ jasmine-standalone-2.4.1/src/fizzBuzz.js
@@ -3,9 +3,43 @@ function Javabuzz(){
 
 
 Javabuzz.prototype = {
-	isDivisibleByThree: function(number) {
-		return number % 3 === 0
-	}
+	isDivisibleBy: function(divisor, number) {
+		return number % divisor === 0
+	},
+
+  checkNumber: function(number) {
+    if (number === 0) {
+      console.log(number)
+      return number
+    }
+    else if (this.isDivisibleBy(15, number)) {
+      console.log('FizzBuzz');
+      return 'FizzBuzz'
+    }
+    else if (this.isDivisibleBy(5, number)) {
+      console.log('Buzz')
+      return 'Buzz'
+    }
+    else if (this.isDivisibleBy(3, number)) {
+      console.log('Fizz')
+      return 'Fizz'
+    }
+    else {
+      console.log(number)
+      return number
+    }
+  }
+
+
 
 
 };
+
+var javabuzz = new Javabuzz();
+var N = 100
+var numbers = Array.apply(null, {length: N + 1}).map(Number.call, Number)
+var val;
+var a = numbers
+for (val of a) {
+  javabuzz.checkNumber(val);
+}
